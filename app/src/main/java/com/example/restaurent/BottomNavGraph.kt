6dbed23aabd2a90d39d9a10e55bc70177450d133
kweche <71312433+Kweche-Ebechue-Divine@users.noreply.github.com>
2022-04-18 00:56:32 +0600

    image issue fix+product details

--- app/src/main/java/com/example/restaurent/BottomNavGraph.kt
+++ app/src/main/java/com/example/restaurent/BottomNavGraph.kt
@@ -2,18 +2,19 @@ package com.example.restaurent
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.restaurent.screen.HomeScreen
-import com.example.restaurent.screen.ProfileScreen
-import com.example.restaurent.screen.RecipeListScreen
-import com.example.restaurent.screen.SettingsScreen
+import androidx.navigation.navArgument
+import com.example.restaurent.model.Recipe
+import com.example.restaurent.screen.*
 
 
 @Composable
 fun BottomNavGraph(navController: NavHostController) {
     NavHost(
         navController = navController,
+
         startDestination = BottomBarScreen.Home.route
     ) {
         composable(route = BottomBarScreen.Home.route) {
@@ -28,7 +29,14 @@ fun BottomNavGraph(navController: NavHostController) {
         composable(
             route = BottomBarScreen.RList.route
         ){
-            RecipeListScreen()
+            RecipeListScreen(navController = navController)
+        }
+        composable(
+            route = BottomBarScreen.ListDetails.route,
+           // arguments = listOf(navArgument("recipe"){type= NavType.ParcelableType(Recipe::class.java)})
+        ){
+            RecipeDetails1()
+
         }
 
     }
