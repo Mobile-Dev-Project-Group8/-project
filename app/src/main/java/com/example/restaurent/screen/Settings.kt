59ce45d7cd65a4a071dd0c3ca50c3a9b0e00ad0f
Shaik-Chapal <70383236+Shaik-Chapal@users.noreply.github.com>
2022-04-21 16:56:54 +0300

    Update Settings.kt
    added new language

--- app/src/main/java/com/example/restaurent/screen/Settings.kt
+++ app/src/main/java/com/example/restaurent/screen/Settings.kt
@@ -39,7 +39,7 @@ fun MyContent(){
 
     var mExpanded by remember { mutableStateOf(false) }
 
-    val mCities = listOf("English", "English", "Spanish", "German" )
+    val mCities = listOf("English", "Finnish", "Spanish", "German" )
 
     var mSelectedText by remember { mutableStateOf("") }
 
